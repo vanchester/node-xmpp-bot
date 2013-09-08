918881ff66781112ccd68560118a9d1bdadfb741
vanchester <admin@vanchester.ru>
2013-09-08 21:55:14 +0700

    ADD lookup plugin

---
+++ plugins/lookup.js
@@ -0,0 +1,23 @@
+var dns = require('dns');
+
+exports.lookup = {
+    name: 'lookup',
+    about: 'DNS lookup',
+    help: 'lookup <DOMAIN>',
+    enabled: 1,
+    run: function(params, stanza, plugins, client) {
+        if (params[0]) {
+            dns.lookup(params[0], function (err, address, family) {
+                if (err) {
+                    stanza.c('body').t('Wrong domain');
+                } else {
+                    stanza.c('body').t(address);
+                }
+                client.send(stanza);
+            });
+            return null;
+        }
+
+        return this.help;
+    }
+};
