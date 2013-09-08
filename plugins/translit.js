1556e33d33b14fe91695ce98cff1b8a8f94f853f
vanchester <admin@vanchester.ru>
2013-09-08 21:39:28 +0700

    ADD translit plugin

---
+++ plugins/translit.js
@@ -0,0 +1,21 @@
+function translite(str){
+    var arr = {'а':'a', 'б':'b', 'в':'v', 'г':'g', 'д':'d', 'е':'e', 'ж':'g', 'з':'z', 'и':'i', 'й':'y', 'к':'k', 'л':'l', 'м':'m', 'н':'n', 'о':'o', 'п':'p', 'р':'r', 'с':'s', 'т':'t', 'у':'u', 'ф':'f', 'ы':'i', 'э':'e', 'А':'A', 'Б':'B', 'В':'V', 'Г':'G', 'Д':'D', 'Е':'E', 'Ж':'G', 'З':'Z', 'И':'I', 'Й':'Y', 'К':'K', 'Л':'L', 'М':'M', 'Н':'N', 'О':'O', 'П':'P', 'Р':'R', 'С':'S', 'Т':'T', 'У':'U', 'Ф':'F', 'Ы':'I', 'Э':'E', 'ё':'yo', 'х':'h', 'ц':'ts', 'ч':'ch', 'ш':'sh', 'щ':'shch', 'ъ':'', 'ь':'', 'ю':'yu', 'я':'ya', 'Ё':'YO', 'Х':'H', 'Ц':'TS', 'Ч':'CH', 'Ш':'SH', 'Щ':'SHCH', 'Ъ':'', 'Ь':'','Ю':'YU', 'Я':'YA'};
+    var replacer = function (a) {
+        return arr[a] || a
+    };
+    return str.replace(/[А-яёЁ]/g,replacer);
+}
+
+exports.translit = {
+    name: 'translit',
+    about: 'Translit russian letters in string',
+    help: 'translit <STRING>',
+    enabled: 1,
+    run: function(params) {
+        if (params[0]) {
+            return translite(params.join(' '));
+        }
+
+        return this.help;
+    }
+};
