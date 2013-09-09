dbef3a19d827cc940ac1c8fc939a490b71b048b6
vanchester <admin@vanchester.ru>
2013-09-10 04:34:52 +0700

    ADD update code command

---
+++ plugins/admin.update.js
@@ -0,0 +1,19 @@
+var exec = require('child_process').exec;
+
+exports.update = {
+    name: 'update',
+    group: 'Administration',
+    about: 'Update bot code from repository',
+    help: 'update',
+    enabled: 1,
+    max_access: 1,
+    run: function(params, stanza, plugins, client) {
+        exec('git pull', function (error, stdout, stderr) {
+            var message = error ? sdterr : stdout;
+
+            stanza.c('body').t(message);
+            client.send(stanza);
+        });
+        return null;
+    }
+};
