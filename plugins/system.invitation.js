2dcd6c62a42f3fddac96adb75a5e46d7cbb3847b
admin@vanchester.ru <admin@vanchester.ru>
2014-02-02 12:50:21 +0700

    FIX subscription

--- plugins/system.invitation.js
+++ plugins/system.invitation.js
@@ -4,16 +4,60 @@ exports.invitation = {
     name: '',
     enabled: 1,
     afterLoad: function (client) {
-        client.on('stanza', function(stanza) {
-            if (stanza.is('presence') && stanza.type == 'subscribe') {
-                this.send(new xmpp.Presence({ to: stanza.from, type: 'subscribed' }).c('show').t('online'));
-                this.send(new xmpp.Presence({ to: stanza.from }).c('show').t('online'));
+        client.on('stanza', function(stanzaOrig) {
+            var stanza = stanzaOrig.clone();
+            if (stanza.is('presence') && stanza.attrs.type == 'subscribe') {
+                var jidFrom = new xmpp.JID(stanza.attrs.from);
+                var jidTo = new xmpp.JID(stanza.attrs.to);
+                var userJID = jidFrom.user + '@' + jidFrom.domain;
+                var botJID = jidTo.user + '@' + jidTo.domain;
+                if (userJID == botJID) {
+                    return;
+                }
+
+                //console.log('send ADD request to server');
+                var addRequest = new xmpp.Iq({
+                    from: botJID,
+                    type: 'set'
+                });
+                addRequest
+                    .c('query', {xmlns: 'jabber:iq:roster'})
+                    .c('item', {jid: userJID, name: userJID, subscription: 'from'})
+                    .c('group').t('Friends');
+
+                //console.log(addRequest.toString());
+
+                client.send(addRequest);
+
+                //console.log('Send presence stanzas');
+                var acceptedSubscription = new xmpp.Presence({
+                    from: stanza.attrs.to,
+                    to: stanza.attrs.from,
+                    type: 'subscribed'
+                });
+                acceptedSubscription.c('show').t('online');
+
+                //console.log(subscribedSubscription.toString());
+                client.send(acceptedSubscription);
+
+                //console.log('send online status');
+                var status = new xmpp.Presence({
+                    'from': botJID,
+                    'to': userJID,
+                    'type': 'subscribe',
+                    'xmlns:stream': 'http://etherx.jabber.org/streams'
+                });
+                status.c('show').t('online');
+
+                //console.log(status.toString());
+                client.send(status);
 
                 try {
                     var body = "Welcome to Bot! Type 'help' to see available commands";
                     if (body) {
                         var message = new xmpp.Message({
-                            to: stanza.from,
+                            from: stanza.attrs.to,
+                            to: stanza.attrs.from,
                             type: "chat",
                             'xmlns:stream': "http://etherx.jabber.org/streams"
                         }).c('body').t(body);
